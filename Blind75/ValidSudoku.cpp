280cfff9b5f7c50b37cc6ce365095faf5fb93052
Aashish-Thapa <aashishthapa520@gmail.com>
2025-02-20 14:41:48 -0600

    solved

---
+++ Blind75/ValidSudoku.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution{
+    public:
+    bool validSudoku(vector<vector<char>>& board){
+        vector<unordered_set<char>> row(9), col(9), box(9);
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char num = board[i][j];
+                if(num == '.'){
+                    continue;
+                }
+                int boxindex = (i / 3)/3 + (j / 3);
+                if(row[i].count(num) || col[j].count(num) || box[boxindex].count(num)){
+                    return false;
+                }
+                row[i].insert(num);
+                col[j].insert(num);
+                box[boxindex].insert(num);
+            }
+        }
+        return true;
+    }
+};
+int main(){
+    return 0;
+}
